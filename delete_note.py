1cb25b06a8f1c8b95c15db9ed81e4e7107c71a59
AnKarabanov <andreykarabanov69@yandex.com>
2025-01-24 21:00:28 +0300

    Update delete_note.py

--- delete_note.py
+++ delete_note.py
@@ -5,23 +5,27 @@ list_ = [{"Имя": "Андрей",
           "Заголовок": "Хоббит"},
          {"Имя": "Максим",
           "Заголовок": "Сталкер"}]
-print("Текущие заметки:")
-for user in list_[:]:
-    print(user)
+# Создаем функцию
+def delete_note_function():
+    print("Текущие заметки:")
+    for user in list_[:]:
+        print(user)
 # При вводе любого имени или заголовка удаляет словарь и выводит оставшиеся
-user_input = input("Введите имя пользователя или заголовок для удаления заметки: ")
+    user_input = input("Введите имя пользователя или заголовок для удаления заметки: ")
 
-for i, book in enumerate(list_):
+    for i, book in enumerate(list_):
 
-    if book["Имя"].lower() == user_input.lower():
-        del list_[i]
-        print("Успешно удалено. Остались следующие заметки:")
-        break
-    if book["Заголовок"].lower() == user_input.lower():
-        del list_[i]
-        print("Успешно удалено. Остались следующие заметки:")
-        break
-else: print("Такого заголовка нет. Ваши заголовки:")
+        if book["Имя"].lower() == user_input.lower():
+            del list_[i]
+            print("Успешно удалено. Остались следующие заметки:")
+            break
+        if book["Заголовок"].lower() == user_input.lower():
+            del list_[i]
+            print("Успешно удалено. Остались следующие заметки:")
+            break
+    else: print("Такого заголовка нет. Ваши заголовки:")
 
-for user in list_[:]:
-    print(user)
+    for user in list_[:]:
+        print(user)
+
+delete_note_function()
